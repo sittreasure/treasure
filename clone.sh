7d21f6d7ecb73005359acb08ceb751f9a72b85a5
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-08 22:26:57 +0700

    [UPDATE] add clone configuration repo

--- clone.sh
+++ clone.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/sittreasure/frontend.git
 git clone https://github.com/sittreasure/fileAPI.git
 git clone https://github.com/sittreasure/mainAPI.git
+git clone https://github.com/sittreasure/configuration.git
