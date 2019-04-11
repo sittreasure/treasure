67d2674aee4206a2d2587b2e7443e9a95fa53685
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-11 09:10:30 +0700

    [UPDATE] delete configuration & discovery & gateway

--- clone.sh
+++ clone.sh
@@ -1,5 +1,3 @@
 git clone https://github.com/sittreasure/frontend.git
 git clone https://github.com/sittreasure/fileAPI.git
 git clone https://github.com/sittreasure/mainAPI.git
-git clone https://github.com/sittreasure/configuration.git
-git clone https://github.com/sittreasure/discovery.git
